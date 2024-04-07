d2df995afc455596ffe2d9650d92628cc7801519
Riwan William Bououli <riwan.b@live.fr>
2024-04-07 13:50:04 +0200

    fixing the pagination

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,82 +1,63 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Pagination from "react-js-pagination"
+import Pagination from "react-js-pagination";
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1)
-  const [charactersPerPage] = useState(100)
+  const [currentPage, setCurrentPage] = useState(1);
+  const charactersPerPage = 100;
 
   useEffect(() => {
     const fetchCharacters = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:3000/characters`
-        );
-        setCharacters(response.data.results);
+        const response = await axios.get(`http://localhost:3000/characters?page=${currentPage}`);
+        const filteredCharacters = response.data.results.filter(character => {
+          return character.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" &&
+                 character.thumbnail.extension !== "gif";
+        });
+        setCharacters(filteredCharacters);
         setIsLoading(false);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
     fetchCharacters();
-  }, []);
-
-  const indexOfLastCharacter = currentPage * charactersPerPage
-  const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage
-  const currentCharacters = characters.slice(
-    indexOfFirstCharacter,
-    indexOfLastCharacter
-  )
+  }, [currentPage]);
 
   const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber)
-  }
+    setCurrentPage(pageNumber);
+  };
 
   return isLoading ? (
-    <p>😩 Loading 😩</p>
+    <p>Loading...</p>
   ) : (
     <div className="main">
       <div className="characters-container">
-        {currentCharacters
-          .filter((character) => {
-            // Filtre les sans photo//
-            return (
-              character.thumbnail.path !==
-              "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
-            );
-          }) // .map sur le reste
-          .map((character) => {
-            const {_id} = character
-            return (
-              <div key={character._id} className="character-card">
-                <h1>{character.name}</h1>
-                <Link
-                  to={`/comics/${_id}`}
-                >
-                  <div className="character-info">
-                    <div className="character-image">
-                      <img
-                        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                        alt={character.name}
-                      />
-                    </div>
-                    <div className="character-description">
-                      <p>{character.description}</p>
-                    </div>
-                  </div>
-                </Link>
+        {characters.map((character) => (
+          <div key={character._id} className="character-card">
+            <h1>{character.name}</h1>
+            <Link to={`/comics/${character._id}`}>
+              <div className="character-info">
+                <div className="character-image">
+                  <img
+                    src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                    alt={character.name}
+                  />
+                </div>
+                <div className="character-description">
+                  <p>{character.description}</p>
+                </div>
               </div>
-            );
-          })}
+            </Link>
+          </div>
+        ))}
       </div>
       <Pagination
         activePage={currentPage}
         itemsCountPerPage={charactersPerPage}
-        totalItemsCount={characters.length}
-        pageRangeDisplayed={5}
+        totalItemsCount={1493}
         onChange={handlePageChange}
         itemClass="page-item"
         linkClass="page-link"
